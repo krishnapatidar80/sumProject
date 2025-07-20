a69bdd8faf9f133fd93988303eb68cbe46b5b4f3
krishnapatidar80 <patidarkrishna188@gmail.com>
2025-07-20 10:08:16 +0530

    sum java

---
+++ sum.java
@@ -0,0 +1,11 @@
+class A
+{
+    public static void main(String[] args) {
+        
+        int num1=10 , num2=20;
+
+        int sum = num1+num2;
+
+        System.out.println("sum is "+sum);
+    }
+}
